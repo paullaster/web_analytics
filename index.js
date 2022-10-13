dd03309ed987a32602cc6da91f2c86e574575806
paullaster <paullasterokoth98@gmail.com>
2022-10-13 19:41:40 +0300

    add array

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener ( 'DOMContentLoaded', userClickBehavior);
-
+let behaviorList = [];
 function userClickBehavior () {
     console.log ('DOM fully loaded and parsed');
     document.addEventListener ( 'click', getClikckedElement);
@@ -9,41 +9,24 @@ function getClikckedElement (event) {
     const tagElement = event.target.tagName.toLowerCase ();
     const tagId = event.target.id;
     const tagClass = event.target.className;
-    analysedUserBehavior (tagElement, tagId, tagClass);
+    const tagTextContent = event.target.textContent;
+    analysedUserBehavior (tagElement, tagId, tagClass, tagTextContent);
 };
 function getElementByID (id) {
     return document.getElementById(id);
 }
 
-function analysedUserBehavior (elment, id, className) {
-    console.log ('clicked element is: ', elment);
-    console.log ('clicked element id is: ', id);
-    console.log ('clicked element class is: ', className);
+function analysedUserBehavior (elment, id, className, elementTextContent) {
+    const clickedElementObject = {
+        elment: elment,
+        id: id,
+        className: className,
+        elementTextContent: elementTextContent
+    };
     if (elment === 'button') {
         getElementByID (id).style.backgroundColor = 'red';
     }
-
-    // create table for user Behavior analysis
-    addElementToDOM ('table', 'analytic_table', 'analytic_tt', 'analysis');
-    addElementToDOM ('tr', 'analytic_tr_headings', 'analytic_tr', 'analytic_table');
-    addElementToDOM ('th', 'analytic_th_tag_name', 'analytic_th', 'analytic_tr_headings');
-    addElementToDOM ('th', 'analytic_th_tag_id', 'analytic_th', 'analytic_tr_headings');
-    addElementToDOM ('th', 'analytic_th_tag_class', 'analytic_th', 'analytic_tr_headings');
-    addElementToDOM ('tr', 'analytic_tr_data', 'analytic_tr', 'analytic_table');
-    addElementToDOM ('td', 'analytic_td_tag_name', 'analytic_td', 'analytic_tr_data');
-    addElementToDOM ('td', 'analytic_td_tag_id', 'analytic_td', 'analytic_tr_data');
-    addElementToDOM ('td', 'analytic_td_tag_class', 'analytic_td', 'analytic_tr_data');
-    
-    // add text content to table
-    //table headins:
-    addTextToElement ('analytic_th_tag_name', "Tag Name");
-    addTextToElement ('analytic_th_tag_id', "Tag ID");
-    addTextToElement ('analytic_th_tag_class', "Tag Class");
-
-    //table data:
-    addTextToElement ('analytic_td_tag_name', elment);
-    addTextToElement ('analytic_td_tag_id', id);
-    addTextToElement ('analytic_td_tag_class', className);
+    elementsClicked (clickedElementObject);
 }
 
 function createElement (elment, id, className, ) {
@@ -61,3 +44,13 @@ function addTextToElement (id, text) {
     getElementByID (id).innerHTML = text;
 }
 
+function elementsClicked (obj) {
+    behaviorList.push (obj);
+    showElementsClicked (behaviorList);
+};
+function showElementsClicked () {
+    behaviorList.forEach ( displayElementsClicked );
+}
+function displayElementsClicked (obj) {
+    console.table (obj);
+}
